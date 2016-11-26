5b0ea4fd0959c99db2be837eedf2b1457afce162
drzel <shayolden@hotmail.com>
2016-11-26 13:21:30 +1100

    refactor command

--- lib/toy_robot/command.rb
+++ lib/toy_robot/command.rb
@@ -1,65 +1,44 @@
-class Command
-  def initialize(robot:, **args)
-    @robot = robot
-    post_initialize args
-  end
-
-  def post_initialize(*); end
-
-  def execute
-    raise NotImplementedError
-  end
-end
-
-class PlaceCommand < Command
-  def post_initialize(command:, arena: NullArena.new)
-    @command = command
-    @arena = arena
+class PlaceCommand
+  def initialize(robot: robot, arena: NullArena.new, command:)
+    @robot  = robot
+    @arena  = arena
+    @params = command[/\s.*/].delete(" ").split(",")
   end
 
   def execute
     position = NullPosition.new arena: @arena
-    position = position.go_to(x:         params[:x],
-                              y:         params[:y],
-                              direction: params[:direction])
+    position = position.go_to(x:         @params[0].to_i,
+                              y:         @params[1].to_i,
+                              direction: @params[2].to_sym)
     @robot.position = position
-  end
-
-  private
-
-  def params
-    ary = @command[/\s.*/].delete(" ").split(",")
-    { x:         ary[0].to_i,
-      y:         ary[1].to_i,
-      direction: ary[2].to_sym }
   end
 end
 
-class MoveCommand < Command
+class MoveCommand
   def execute
     @robot.move
   end
 end
 
-class LeftCommand < Command
+class LeftCommand
   def execute
     @robot.left
   end
 end
 
-class RightCommand < Command
+class RightCommand
   def execute
     @robot.right
   end
 end
 
-class ReportCommand < Command
+class ReportCommand
   def execute
     @robot.report
   end
 end
 
-class InvalidCommand < Command
+class InvalidCommand
   def execute
     puts "Invalid command"
   end
