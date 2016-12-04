5970a203da18e84351244f10cf69f20a95eea83e
drzel <shayolden@hotmail.com>
2016-12-04 15:10:33 +1100

    exit command will still work if preceded by whitespace (mimicking unix commands)

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -15,7 +15,7 @@ module ToyRobot
     loop do
       print "Input command: "
       input = gets
-      break if input =~ /^exit\s+/
+      break if input =~ /^\s*exit\s+/
       client.command_for Input.new(input)
     end
   end
