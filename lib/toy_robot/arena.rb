6863cc25066660deb90de56de11f85da21b90546
drzel <shayolden@hotmail.com>
2016-12-04 15:20:32 +1100

    refactor arena, don't use lonely operator, makes compatible with Ruby >= 1.9

--- lib/toy_robot/arena.rb
+++ lib/toy_robot/arena.rb
@@ -1,20 +1,11 @@
 class Arena
   def initialize(width:, height:)
-    @width  = width
-    @height = height
+    @max_x = width - 1
+    @max_y = height - 1
   end
 
   def inbounds?(x_coord, y_coord)
-    x_coord&.between?(0, max_x) && y_coord&.between?(0, max_y)
-  end
-
-  private
-
-  def max_x
-    @width - 1
-  end
-
-  def max_y
-    @height - 1
+    x_coord && x_coord.between?(0, @max_x) &&
+    y_coord && y_coord.between?(0, @max_y)
   end
 end
