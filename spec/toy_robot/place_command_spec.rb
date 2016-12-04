166829907d7a153a8932ee67035701f121e4312e
drzel <shayolden@hotmail.com>
2016-12-05 00:25:47 +1100

    reorganise place command spec

--- spec/toy_robot/place_command_spec.rb
+++ spec/toy_robot/place_command_spec.rb
@@ -4,54 +4,6 @@ describe PlaceCommand do
   let(:robot) { build :robot }
 
   describe "#execute" do
-    context 'when @params[2] is "n"' do
-      let(:command) do
-        build :place_command, target: robot, params: ["0", "0", "n"]
-      end
-
-      it "creates a new position facing north" do
-        expect(Position).to receive(:new).with hash_including(direction: North)
-        allow(command.target).to receive :place
-        command.execute
-      end
-    end
-
-    context 'when @params[2] is "e"' do
-      let(:command) do
-        build :place_command, target: robot, params: ["0", "0", "e"]
-      end
-
-      it "creates a new position facing east" do
-        expect(Position).to receive(:new).with hash_including(direction: East)
-        allow(command.target).to receive :place
-        command.execute
-      end
-    end
-
-    context 'when @params[2] is "s"' do
-      let(:command) do
-        build :place_command, target: robot, params: ["0", "0", "s"]
-      end
-
-      it "creates a new position facing south" do
-        expect(Position).to receive(:new).with hash_including(direction: South)
-        allow(command.target).to receive :place
-        command.execute
-      end
-    end
-
-    context 'when @params[2] is "w"' do
-      let(:command) do
-        build :place_command, target: robot, params: ["0", "0", "w"]
-      end
-
-      it "creates a new position facing west" do
-        expect(Position).to receive(:new).with hash_including(direction: West)
-        allow(command.target).to receive :place
-        command.execute
-      end
-    end
-
     context "when params are valid" do
       let(:command) do
         build :place_command, target: robot, params: ["0", "0", "n"]
@@ -63,13 +15,63 @@ describe PlaceCommand do
         expect(command.target).to receive(:place).with dbl
         command.execute
       end
+
+      context 'when @params[2] is "n"' do
+        let(:command) do
+          build :place_command, target: robot, params: ["0", "0", "n"]
+        end
+
+        it "creates a new position facing north" do
+          expect(Position).to receive(:new).with(
+            hash_including(direction: North),
+          )
+          allow(command.target).to receive :place
+          command.execute
+        end
+      end
+
+      context 'when @params[2] is "e"' do
+        let(:command) do
+          build :place_command, target: robot, params: ["0", "0", "e"]
+        end
+
+        it "creates a new position facing east" do
+          expect(Position).to receive(:new).with hash_including(direction: East)
+          allow(command.target).to receive :place
+          command.execute
+        end
+      end
+
+      context 'when @params[2] is "s"' do
+        let(:command) do
+          build :place_command, target: robot, params: ["0", "0", "s"]
+        end
+
+        it "creates a new position facing south" do
+          expect(Position).to receive(:new).with(
+            hash_including(direction: South),
+          )
+          allow(command.target).to receive :place
+          command.execute
+        end
+      end
+
+      context 'when @params[2] is "w"' do
+        let(:command) do
+          build :place_command, target: robot, params: ["0", "0", "w"]
+        end
+
+        it "creates a new position facing west" do
+          expect(Position).to receive(:new).with hash_including(direction: West)
+          allow(command.target).to receive :place
+          command.execute
+        end
+      end
     end
-  end
 
-  context "with invalid params" do
-    let(:command) { build :place_command, params: ["derp"] }
+    context "when params are invalid" do
+      let(:command) { build :place_command, params: ["derp"] }
 
-    describe "#execute" do
       it "creates a new InvalidCommand" do
         dbl = double
         expect(InvalidCommand).to receive(:new) { dbl }
